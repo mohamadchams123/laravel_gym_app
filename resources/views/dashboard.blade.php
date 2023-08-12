52af64f89334a84f928fc0b949f1b3022c801d38
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-12 19:15:43 +0300

    tozbit links lal pages w layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,43 +1,46 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex font-semibold text-xl text-gray-900 dark:text-gray-200 leading-tight">
-            <h2 class="m-5">
-                <a href="#todo">{{ __('Exercices') }}</a>
-            </h2>
-            <h2 class="m-5">
-                <a href="#sh">{{ __('Shop') }}</a>
-            </h2>
-            <h2 class="m-5">
-                <a href="#sub">{{ __('Subscription') }}</a>
-            </h2>
-            <h2 class="m-5">
-                <a href="#cm">{{ __('Feedback') }}</a>
-            </h2>
+        <div class="flex font-semibold text-xl leading-tight">
+            <p class="m-5"><x-nav-link href="#todo">
+                {{ __('Exercices') }}
+            </x-nav-link></p>
+            <p class="m-5"><x-nav-link href="#sh">
+                {{ __('Shop') }}
+            </x-nav-link></p>
+            <p class="m-5"><x-nav-link href="#sub">
+                {{ __('Subscription') }}
+            </x-nav-link></p>
+            <p class="m-5"><x-nav-link href="#cm">
+                {{ __('Feedback') }}
+            </x-nav-link></p>
         </div>
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h2 id="todo" class="font-semibold text-xl text-gray-900 dark:text-gray-200 pb-5 underline underline-offset-2">
+                    {{-- <h2 id="todo" class="font-semibold text-xl text-gray-900 dark:text-gray-200 pb-5 underline underline-offset-2">
                         <a href="{{route('exercices')}}">{{ __('Learn New Exercices') }}</a>
-                    </h2>
+                    </h2> --}}
+                    <p id="todo" class="mb-5"><x-nav-link class="font-semibold text-xl" href="{{route('exercices')}}">
+                        {{ __('# Learn New Exercices') }}
+                    </x-nav-link></p>
                     <p>Engaging in a diverse range of exercises at the gym offers a multitude of physical and mental benefits that contribute to overall well-being. Whether you're lifting weights, doing cardiovascular workouts, or practicing yoga, these exercises collectively provide a holistic approach to fitness.
                     Strength training exercises, such as weightlifting, help to build and tone muscles while enhancing bone density. This not only leads to improved physical appearance but also increases metabolic rate, allowing the body to burn more calories even at rest. Cardiovascular exercises like running, cycling, or swimming improve heart health, enhance lung capacity, and boost endurance. This cardiovascular conditioning contributes to a healthier cardiovascular system and aids in weight management.
                     Incorporating flexibility exercises, such as yoga or stretching routines, helps maintain joint mobility, reduce the risk of injury, and improve overall flexibility. These exercises also promote relaxation and stress relief, benefiting mental health. Additionally, functional exercises like squats, lunges, and planks enhance overall stability and balance, leading to better posture and reduced risk of falls, especially as we age.
                     Consistently engaging in a variety of exercises fosters discipline and a positive mindset. Setting and achieving fitness goals provides a sense of accomplishment, boosting self-esteem and self-confidence. The release of endorphins during exercise contributes to improved mood and reduced stress, anxiety, and depression.
                     In essence, the diverse array of exercises available in a gym setting delivers a comprehensive approach to fitness that encompasses strength, endurance, flexibility, and mental well-being. The combination of these exercises not only promotes physical health but also instills a sense of vitality, confidence, and balance in our daily lives.</p>
-                    <h2 id="sh" class="font-semibold text-xl text-gray-900 dark:text-gray-200 border-t pt-10 mt-10 pb-5 underline underline-offset-2">
-                        <a href="{{route('shop')}}">{{ __('Check Out Our Shop') }}</a>
-                    </h2>
+                    <p id="sh" class="mb-5 border-t pt-10 mt-10"><x-nav-link class="font-semibold text-xl" href="{{route('shop')}}">
+                        {{ __('# Check Out Our Shop') }}
+                    </x-nav-link></p>
                     <p>
                     Welcome to our boutique gym equipment and accessories shop, where fitness enthusiasts and health-conscious individuals find everything they need to elevate their workout experience. Our shop is a haven for those seeking top-quality fitness equipment and accessories that seamlessly blend functionality, style, and durability. From cutting-edge resistance bands and ergonomic yoga mats to premium dumbbells and sleek water bottles, our curated collection caters to every fitness regimen and lifestyle.
                     Step into a world of innovation and excellence, where each item is handpicked to enhance your fitness journey. Our shop prides itself on offering a range of products that cater to beginners embarking on their fitness quest as well as seasoned athletes looking to elevate their routines. Whether you're setting up a home gym or need that extra touch of style during your gym sessions, we have you covered.
                     Our passionate and knowledgeable staff are here to guide you through our selection, ensuring you find the perfect gear to complement your goals. We believe that the right equipment and accessories not only amplify your workouts but also inspire you to push your boundaries and achieve your personal best. At our gym equipment and accessories shop, we're not just selling products – we're empowering you to embrace a healthier lifestyle with enthusiasm and confidence.
                     </p>
-                    <h2 id="sub" class="font-semibold text-xl text-gray-900 dark:text-gray-200 border-t pt-10 mt-10 pb-5 underline underline-offset-2">
-                        <a href="{{route('subscription')}}">{{ __('Get A Membership') }}</a>
-                    </h2>
+                    <p id="sub" class="mb-5 border-t pt-10 mt-10"><x-nav-link class="font-semibold text-xl" href="{{route('subscription')}}">
+                        {{ __('# Get A Membership') }}
+                    </x-nav-link></p>
                     <p>Subscribing to a gym membership offers a plethora of benefits that extend far beyond the physical gains. It's an investment in your overall well-being, providing a comprehensive approach to health and self-improvement. Here are some of the key benefits you can derive from a gym subscription:
                     <br>
                     1.Physical Fitness: A gym membership provides access to a variety of exercise equipment and facilities, allowing you to engage in diverse workout routines that target different muscle groups. This helps you build strength, endurance, flexibility, and cardiovascular health.
@@ -54,9 +57,9 @@
                     11.Better Sleep: Engaging in regular physical activity can improve the quality of your sleep, helping you fall asleep faster and enjoy deeper rest.<br>
                     12.Long-Term Habits: Joining a gym encourages the development of consistent exercise habits. Over time, these habits become part of your lifestyle, promoting long-term health and vitality.<br>
                     Ultimately, a gym subscription offers more than just access to exercise equipment; it provides you with the tools, support, and motivation to embark on a holistic journey toward improved physical, mental, and emotional well-being.</p>
-                    <h2 id="cm" class="font-semibold text-xl text-gray-900 dark:text-gray-200 border-t pt-10 mt-10 pb-5 underline underline-offset-2">
-                        <a href="{{route('feedback')}}">{{ __('Give Us A Feedback') }}</a>
-                    </h2>
+                    <p id="cm" class="mb-5 border-t pt-10 mt-10"><x-nav-link class="font-semibold text-xl" href="{{route('feedback')}}">
+                        {{ __('# Give Us Your Feedback') }}
+                    </x-nav-link></p>
                     <p>We value your input! Your feedback holds the power to shape the future of our gym app and enhance the experience for everyone. Whether it's a suggestion, a feature request, or simply sharing your thoughts, your voice matters. By providing us with your valuable insights, you're contributing to a community-driven platform that aims to meet your needs and exceed your expectations. Remember, your feedback not only helps us understand what's working but also highlights areas where we can improve. Together, we can create a gym app that truly resonates with you and enhances your fitness journey. Share your thoughts today and be a part of our mission to make every workout count!</p>
                 </div>
             </div>
