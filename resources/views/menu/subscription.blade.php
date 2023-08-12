9e57f14d5fa14fb57a6e585dd9896b592a098bad
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-12 19:28:12 +0300

    using nav link

--- resources/views/menu/subscription.blade.php
+++ resources/views/menu/subscription.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout>
-    
+    <x-slot name="header">
+        <div class="flex font-semibold text-xl leading-tight m-5">
+            <x-nav-link active="true">
+                {{ __('Subscription') }}
+            </x-nav-link>
+        </div>
+    </x-slot>
 </x-app-layout>
