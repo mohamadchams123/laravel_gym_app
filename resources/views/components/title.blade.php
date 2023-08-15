d9945a98f731170753c08e85be78ab1cf033db55
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-15 19:06:16 +0300

    tozbit subscription page #2

---
+++ resources/views/components/title.blade.php
@@ -0,0 +1,3 @@
+<p {{ $attributes->merge(['class' => 'block font-medium text-md text-gray-700 dark:text-gray-300']) }}>
+    {{ $slot }}
+</p>
--- resources/views/menu/subscription.blade.php
+++ resources/views/menu/subscription.blade.php
@@ -9,42 +9,44 @@
         <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="px-12 py-6 text-gray-900 dark:text-gray-100">
-                    <h1>Subscription Page</h1>
+                    <x-title class="text-xl mb-6">Subscription Page</x-title>
                     <div>
                         @if ($user->subscription)
-                            <p>You are subscribed!</p>
-                            <table>
+                            <x-title>You are subscribed!</x-title>
+                            <div class="flex justify-center">
+                            <table class="text-center mt-6">
                                 <tr>
                                     <td>
-                                        Subscription Start Date:
+                                        <x-title>Subscription Start Date:</x-title>
                                     </td>
                                     <td>
-                                        {{ $user->subscription_start_date }}
+                                        <time>{{ $user->subscription_start_date }}</time>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
-                                        Subscription End Date:
+                                        <x-title>Subscription End Date:</x-title>
                                     </td>
                                     <td>
-                                        {{ $user->subscription_end_date }}
+                                        <time>{{ $user->subscription_end_date }}</time>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td colspan="2">
                                         <form action="{{ route('unsubscribe') }}" method="POST">
                                             @csrf
-                                            <x-primary-button>Unsubscribe</x-primary-button>
+                                            <x-primary-button onclick="return confirm('Are you sure you want to unsubscribe?');">Unsubscribe</x-primary-button>
                                         </form>
                                     </td>
                                 </tr>
                             </table>
+                            </div>
                         @else
-                            <h1>Payment Details</h1>
+                            <x-title>Payment Details</x-title>
                             <div class="flex justify-center">
                             <form action="{{ route('subscribe') }}" method="POST">
                                 @csrf
-                                <table class="text-right">
+                                <table class="text-right mt-6">
                                     <tr>
                                         <td><x-input-label for="message" :value="__('Card Number')" /></td>
                                         <td>
