c508387329654b64fc23d95d855acb9ca1903c15
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-15 16:01:20 +0300

    tozbit subscription page (not done)

--- resources/views/menu/subscription.blade.php
+++ resources/views/menu/subscription.blade.php
@@ -6,57 +6,83 @@
     </x-slot>
 
     <div class="p-12">
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg text-center">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="px-12 py-6 text-gray-900 dark:text-gray-100">
                     <h1>Subscription Page</h1>
                     <div>
                         @if ($user->subscription)
                             <p>You are subscribed!</p>
-                            <p>Subscription Start Date: {{ $user->subscription_start_date }}</p>
-                            <p>Subscription End Date: {{ $user->subscription_end_date }}</p>
-                            <form action="{{ route('unsubscribe') }}" method="POST">
-                                @csrf
-                                <x-primary-button>Unsubscribe</x-primary-button>
-                            </form>
+                            <table>
+                                <tr>
+                                    <td>
+                                        Subscription Start Date:
+                                    </td>
+                                    <td>
+                                        {{ $user->subscription_start_date }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Subscription End Date:
+                                    </td>
+                                    <td>
+                                        {{ $user->subscription_end_date }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <form action="{{ route('unsubscribe') }}" method="POST">
+                                            @csrf
+                                            <x-primary-button>Unsubscribe</x-primary-button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
                         @else
                             <h1>Payment Details</h1>
-                        {{-- {!! Form::open(['route' => 'subscribe']) !!}
-<div class="form-group">
-    {!! Form::label('card_number', 'Card Number') !!}
-    {!! Form::text('card_number', null, ['class' => 'form-control', 'placeholder' => 'Enter card number']) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::label('expiry_date', 'Expiry Date') !!}
-    {!! Form::text('expiry_date', null, ['class' => 'form-control', 'placeholder' => 'MM/YY']) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::label('cvv', 'CVV') !!}
-    {!! Form::text('cvv', null, ['class' => 'form-control', 'placeholder' => 'Enter CVV']) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::label('cardholder_name', 'Cardholder Name') !!}
-    {!! Form::text('cardholder_name', null, ['class' => 'form-control', 'placeholder' => 'Enter cardholder name']) !!}
-</div>
-{!! Form::close() !!} --}}
+                            <div class="flex justify-center">
                             <form action="{{ route('subscribe') }}" method="POST">
                                 @csrf
-                                <x-primary-button>Subscribe</x-primary-button>
+                                <table class="text-right">
+                                    <tr>
+                                        <td><x-input-label for="message" :value="__('Card Number')" /></td>
+                                        <td>
+                                            <x-text-input type="text" id="card_number" name="card_number" placeholder="Enter card number" class="m-1 block w-full" required />
+                                            <x-input-error class="mt-2" :messages="$errors->get('card_number')" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><x-input-label for="message" :value="__('Expiry Date')" /></td>
+                                        <td>
+                                            <x-text-input type="text" id="expiry_date" name="expiry_date" placeholder="MM/YY" class="m-1 block w-full" required />
+                                            <x-input-error class="mt-2" :messages="$errors->get('expiry_date')" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><x-input-label for="message" :value="__('CVV')" /></td>
+                                        <td>
+                                            <x-text-input type="text" id="cvv" name="cvv" placeholder="Enter CVV" class="m-1 block w-full" required />
+                                            <x-input-error class="mt-2" :messages="$errors->get('cvv')" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><x-input-label for="message" :value="__('Card Holder Name')" /></td>
+                                        <td>
+                                            <x-text-input type="text" id="cardholder_name" name="cardholder_name" placeholder="Enter cardholder name" class="m-1 block w-full" required />
+                                            <x-input-error class="mt-2" :messages="$errors->get('cardholder_name')" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2"><x-primary-button class="m-1">Subscribe</x-primary-button></td>
+                                    </tr>
+                                </table>
                             </form>
+                            </div>
                         @endif
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-        document.querySelector('form').addEventListener('submit', function() {
-            alert('Subscribed successfully!');
-        });
-    </script>
-
 </x-app-layout>
