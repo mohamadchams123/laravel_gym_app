7b3c0d0b6de718aacd8229f15304f812fbf8f957
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-21 17:15:25 +0300

    sdfgs

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable
     }
     public function userCards()
     {
-        return $this->hasOne(UserCard::class);
+        return $this->hasMany(UserCard::class);
     }
     public function cart()
     {
