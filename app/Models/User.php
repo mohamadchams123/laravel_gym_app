66c9202b78090a2d4bb09a9d394d77332df884df
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-22 00:03:42 +0300

    Revert "Merge branch 'main' of https://github.com/mohamadchams123/laravel_gym_app"
    This reverts commit 0fe89a9e4361f9ab5b1ee327f7131c8ce43e4366, reversing
    changes made to 39e0669b28971410c651166b2c7b4209e88b977a.

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable
     }
     public function userCards()
     {
-        return $this->hasMany(UserCard::class);
+        return $this->hasOne(UserCard::class);
     }
     public function cart()
     {
