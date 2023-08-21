b1d7d8c6de781bfc6f024829bc8dc4cb974353ca
mohamad chamseddine <mohamadchams123@gmail.com>
2023-08-22 00:02:00 +0300

    Revert "ghghgh"
    This reverts commit 040d63956f1ba4404db6fed37a60a714b0fe61db.

--- resources/views/workouts/create.blade.php
+++ resources/views/workouts/create.blade.php
@@ -1,42 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Shoulder Workouts</title>
-</head>
-<body>
-    <form method="POST" action="{{ route('shoulder-workouts') }}">
-        @csrf
-        <input type="text" name="category_name" placeholder="Category">
-        <input type="text" name="exercise_name" placeholder="Exercise">
-        <input type="text" name="exercise_info" placeholder="Exercise Info">
-        <input type="text" name="exercise_howto" placeholder="Exercise How-to">
-        <input type="text" name="exercise_sr" placeholder="Exercise SR">
+<!-- resources/views/workout/create.blade.php -->
 
-        <div id="mistake-container">
-            <div class="mistake">
-                <input type="text" name="mistakes[0][name]" placeholder="Mistake Name">
-                <input type="text" name="mistakes[0][description]" placeholder="Mistake Description">
-            </div>
+<form method="POST" action="{{ route('workouts.store') }}">
+    @csrf
+    <input type="text" name="category_name" placeholder="Category">
+    <input type="text" name="exercise_name" placeholder="Exercise">
+    <input type="text" name="exercise_info" placeholder="Exercise Info">
+    <input type="text" name="exercise_howto" placeholder="Exercise How-to">
+    <input type="text" name="exercise_sr" placeholder="Exercise SR">
+
+    <div id="mistake-container">
+        <div class="mistake">
+            <input type="text" name="mistakes[][name]" placeholder="Mistake Name">
+            <input type="text" name="mistakes[][description]" placeholder="Mistake Description">
         </div>
+    </div>
 
-        <button type="button" id="add-mistake">Add Mistake</button>
-        <button type="submit">Create Workout</button>
-    </form>
+    <button type="button" id="add-mistake">Add Mistake</button>
+    <button type="submit">Create Workout</button>
+</form>
 
-    <script>
-    document.getElementById('add-mistake').addEventListener('click', function() {
-        var container = document.getElementById('mistake-container');
-        var mistakes = container.getElementsByClassName('mistake');
-        var newIndex = mistakes.length;
-
-        var mistakeDiv = document.createElement('div');
-        mistakeDiv.className = 'mistake';
-        mistakeDiv.innerHTML = `
-            <input type="text" name="mistakes[${newIndex}][name]" placeholder="Mistake Name">
-            <input type="text" name="mistakes[${newIndex}][description]" placeholder="Mistake Description">
-        `;
-        container.appendChild(mistakeDiv);
-    });
-    </script>
-</body>
-</html>
+<script>
+document.getElementById('add-mistake').addEventListener('click', function() {
+    var container = document.getElementById('mistake-container');
+    var mistakeDiv = document.createElement('div');
+    mistakeDiv.className = 'mistake';
+    mistakeDiv.innerHTML = `
+        <input type="text" name="mistakes[][name]" placeholder="Mistake Name">
+        <input type="text" name="mistakes[][description]" placeholder="Mistake Description">
+    `;
+    container.appendChild(mistakeDiv);
+});
+</script>
